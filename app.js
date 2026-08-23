6282f12913247562c86946f18ecb17930cbb758f
Artyom <artemkaa222@mail.ru>
2026-08-23 22:27:28 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const tg = window.Telegram.WebApp;
-const API_URL = 'http://d1.aurorix.net:26312';
+const API_URL = 'https://api.fantastworld.ru:26312';
 
 let currentNick = '';
 let confirmTimer = null;
